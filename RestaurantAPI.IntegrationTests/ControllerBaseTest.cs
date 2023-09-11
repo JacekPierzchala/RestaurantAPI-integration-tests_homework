ee67384666fc3dc2ca919b5eeb2613ce6997f590
Jacek Placek <jacekpierzchala12@gmail.com>
2023-09-11 19:05:59 +0200

    Add project files.

---
+++ RestaurantAPI.IntegrationTests/ControllerBaseTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantAPI.Entities;
+using System.Linq;
+using System.Net.Http;
+using Xunit;
+
+namespace RestaurantAPI.IntegrationTests
+{
+    public class ControllerBaseTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        protected HttpClient _client;
+        protected WebApplicationFactory<Startup> _factory;
+        public ControllerBaseTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory
+             .WithWebHostBuilder(builder =>
+             {
+                 builder.ConfigureServices(services =>
+                 {
+                     var dbContextOptions = services
+                         .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<RestaurantDbContext>));
+
+                     services.Remove(dbContextOptions);
+
+                     services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+
+                     services.AddMvc(option => option.Filters.Add(new FakeUserFilter()));
+
+
+                     services
+                      .AddDbContextFactory<RestaurantDbContext>(options => options.UseInMemoryDatabase("RestaurantDb"),ServiceLifetime.Transient);
+
+                 });
+             });
+
+            _client = _factory.CreateClient();
+        }
+
+        protected void SeedEntity<T>(T Entity)
+            where T:class
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var _dbContext = scope.ServiceProvider.GetService<RestaurantDbContext>();
+
+            _dbContext.Set<T>().Add(Entity);
+            _dbContext.SaveChanges();
+        }
+
+
+    }
+}
---
+++ RestaurantAPI.IntegrationTests/DishControllerTests.cs
@@ -0,0 +1,175 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantAPI.Entities;
+using RestaurantAPI.IntegrationTests.Helpers;
+using RestaurantAPI.Models;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestaurantAPI.IntegrationTests
+{
+    public class DishControllerTests : ControllerBaseTest
+    {
+        public DishControllerTests(WebApplicationFactory<Startup> factory)
+            :base(factory) { }
+
+        [Fact]
+        public async Task CreateDish_WithValidModel_ReturnsCreatedStatus()
+        {
+            // arrange
+            var restaurantId = 1;
+            var model = new CreateDishDto()
+            {
+                 Description= "Test",
+                 Name= "Test",
+                 Price=9.9m,
+                 RestaurantId= restaurantId
+            };
+
+            var httpContent = model.ToJsonHttpContent();
+
+            // act
+            var response = await _client.PostAsync($"/api/restaurant/{restaurantId}/dish", httpContent);
+
+            // arrange 
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+            response.Headers.Location.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task CreateDish_WithInvalidModel_ReturnsBadRequest()
+        {
+            // arrange
+            var restaurantId = 1;
+
+            var model = new CreateDishDto()
+            {
+                Description = "Test",
+                Price = 9.9m,
+                RestaurantId = restaurantId
+            };
+
+            var httpContent = model.ToJsonHttpContent();
+
+            // act
+            var response = await _client.PostAsync($"/api/restaurant/{restaurantId}/dish", httpContent);
+
+            // arrange
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Delete_AllDishesForRestaurant_ReturnsNoContent()
+        {
+            // arrange
+
+            var restaurant = new Restaurant()
+            {
+                CreatedById = 1,
+                Name = "Test"
+            };
+            SeedEntity(restaurant);
+
+            var dish = new Dish()
+            {
+                Description = "Test",
+                Price = 9.9m,
+                RestaurantId = restaurant.Id
+            };
+
+            SeedEntity(dish);
+
+            // act
+            var response = await _client.DeleteAsync($"/api/restaurant/{restaurant.Id}/dish");
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Delete_AllDishesForNonExistingRestaurant_ReturnsNotFound()
+        {
+            // arrange
+            var restaurantId = 10;
+     
+            // act
+            var response = await _client.DeleteAsync($"/api/restaurant/{restaurantId}/dish");
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task GetAllDishes_ForRestaurant_ReturnsOkResult()
+        {
+            // arrange
+            var restaurantId = 1;
+
+            // act
+            var response = await _client.GetAsync($"/api/restaurant/{restaurantId}/dish");
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetAllDishes_ForNonExistingRestaurant_ReturnsNotFoundResult()
+        {
+            // arrange
+            var restaurantId = 100;
+
+            // act
+            var response = await _client.GetAsync($"/api/restaurant/{restaurantId}/dish");
+            
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task GetDish_ForGivenRestaurant_ReturnsOkResult()
+        {
+            // arrange
+
+            var restaurant = new Restaurant()
+            {
+                CreatedById = 1,
+                Name = "Test"
+            };
+            SeedEntity(restaurant);
+
+            var dish = new Dish()
+            {
+                Description = "Test",
+                Price = 9.9m,
+                RestaurantId = restaurant.Id
+            };
+
+            SeedEntity(dish);
+
+            // act
+            var response = await _client.GetAsync($"/api/restaurant/{restaurant.Id}/dish/{dish.Id}");
+                       
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Theory]
+        [InlineData(1, 1000)]
+        [InlineData(100, 2)]
+        public async Task GetDish_ForGivenRestaurant_ReturnsNotFoundResult(int restaurantId, int dishId)
+        {
+            // act
+            var response = await _client.GetAsync($"/api/restaurant/{restaurantId}/dish/{dishId}");
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        }
+
+
+    }
+}
